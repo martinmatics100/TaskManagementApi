94fd288e7fb95c71970b7f52faa712341ac58ec3
Martinmatics100 <martinchukwuemeka67@gmail.com>
2023-09-14 14:16:49 +0100

    commit changes

--- Taskmanagement/TaskManagement.Api/TaskManagement.Data/Entities/User.cs
+++ Taskmanagement/TaskManagement.Api/TaskManagement.Data/Entities/User.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,8 @@ namespace TaskManagement.Data.Entities
     {
         public string UserId { get; set; }
         public string Name { get; set; }
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
         public List<CustomTask> CreatedTasks { get; set; }
         public List<Notification> Notifications { get; set; }
